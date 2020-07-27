14b5cd6a05ebf5301da2c4a54d0de02a9acb75bb
Bea Malsky <beamalsky@datamade.us>
2020-07-27 16:51:27 -0500

    Add header image

--- src/components/config.js
+++ src/components/config.js
@@ -2,9 +2,7 @@ var config = {
     style: 'mapbox://styles/apidae/ckba8sfpn01031irr88z2p3pz',
     showMarkers: true,
     theme: 'light',
-    alignment: 'center',
-    title: 'What happened on June 1 in Cicero?',
-    subtitle: 'Dek TK'
+    alignment: 'center'
 };
 
 export default config
